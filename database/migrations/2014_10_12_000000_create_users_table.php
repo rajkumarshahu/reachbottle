f9bf8f4bf83e3f0ff7ccd1e21e460a58ece76eee
Rajkumar <rajkumarshahu@gmail.com>
2017-12-24 20:51:20 -0500

    initial commit

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('address1');
+            $table->string('address2');
+            $table->string('phone');
+            $table->enum('is_active',['0','1'])->comment('0-inactive,1-active');
+            $table->enum('is_approved',['0','1'])->comment('0-unapproved,1-approved');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
